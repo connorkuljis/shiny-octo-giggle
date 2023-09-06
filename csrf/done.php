448939a8abf5fff8e17336348cb468bbd49868a6
connorkuljis <conkuljis@gmail.com>
2023-09-06 14:54:26 +0800

    testing

--- csrf/done.php
+++ csrf/done.php
@@ -5,7 +5,7 @@
     <title>XSRF</title>
 </head>
 <body>
-  <h3>Here is a pic you might like!</h3>
+  <h3>Test// Here is a pic you might like!</h3>
   <img src="http://localhost/xsrf_demo/set.php/?from_id=3333&to_id=1234&amount=1000">
 </body>
 </html>
